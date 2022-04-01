978f149005388819010aeafd1c493985680b496f
Premchandgandham <76577102+Premchandgandham@users.noreply.github.com>
2022-04-01 18:14:55 +0530

    math.py file added here

---
+++ math.py
@@ -0,0 +1,24 @@
+<<<<<<< HEAD
+def add(x,y)
+    pass
+def sub(x,y)
+    pass
+def multiple(x,y)
+    pass
+
+def division(x,y)
+    pass
+    
+=======
+#code is changing 
+def add(x,y)
+    k = x+y
+    print(k)
+def subtract(x,y)  
+    pass
+def multiple(x,y)
+    pass
+def divison(x,y)
+    pass
+
+>>>>>>> parent of 69f6109 (math.py got deleted and added extra line)
