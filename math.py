908a6a9e8daaf7e970c32070537545c84020b5c3
leelapremchand_gandham <leela_gandham@persistent.com>
2022-04-01 18:17:27 +0530

    modifed math.py in local repo

--- math.py
+++ math.py
@@ -1,16 +1,4 @@
-<<<<<<< HEAD
-def add(x,y)
-    pass
-def sub(x,y)
-    pass
-def multiple(x,y)
-    pass
 
-def division(x,y)
-    pass
-    
-=======
-#code is changing 
 def add(x,y)
     k = x+y
     print(k)
@@ -21,4 +9,4 @@ def multiple(x,y)
 def divison(x,y)
     pass
 
->>>>>>> parent of 69f6109 (math.py got deleted and added extra line)
+
